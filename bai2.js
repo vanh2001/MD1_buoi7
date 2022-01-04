b6a5382d01af6941546e0f3a9d1a6ff769edbbb7
vanh2001 <anhviet1709@gmail.com>
2022-01-04 22:38:56 +0700

    Add file bai 2

---
+++ bai2.js
@@ -0,0 +1,7 @@
+var inputWidth, inputHeight;
+inputWidth = prompt('Enter the width: ');
+inputHeight = prompt('Enter the height: ');
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+var area = width * height ;
+document.write('The area is: '+ area);
