c79db17f1bc68973cf06237c32ce83fbe2750253
vanh2001 <anhviet1709@gmail.com>
2022-01-04 22:39:54 +0700

    Add file bai 3

---
+++ bai3_3.js
@@ -0,0 +1,6 @@
+let banKinh;
+banKinh = prompt('Mời bạn nhập bán kính đường tròn: ', 'banKinh');
+let r = parseInt(banKinh);
+let pi = 3.14;
+let S = pi * r * r;
+document.write('Diện tích đường tròn là: ' + S);
---
+++ bai3_4.js
@@ -0,0 +1,4 @@
+let banKinh = prompt('Mời bạn nhập bán kính hình tròn: ');
+let r = parseInt(banKinh);
+let p = 2 * r;
+document.write('Chu vi hình tròn là: ', + p);
